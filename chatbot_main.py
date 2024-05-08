d19349bea376248f7edd4644d72a4d43457fbf67
Umasha Kaumadi <105776548+umasha2000@users.noreply.github.com>
2024-05-08 12:35:04 +0530

    lemmetizer intialized

--- chatbot_main.py
+++ chatbot_main.py
@@ -6,3 +6,11 @@ import nltk
 
 from nltk.stem import WordNetLemmatizer
 from keras.models import load_model
+
+lemmatizer = WordNetLemmatizer()
+
+
+words = pickle.load(open('words.pkl', 'rb'))
+classes = pickle.load(open('classes.pkl', 'rb'))
+model = load_model('chatbot_model.h5')
+
