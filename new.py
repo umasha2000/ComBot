364f64b850a9ed8b4d1249a859131aeff9a63324
Umasha Kaumadi <105776548+umasha2000@users.noreply.github.com>
2024-05-08 12:48:58 +0530

    Intent loop method added

--- new.py
+++ new.py
@@ -15,3 +15,22 @@ words = []
 classes = []
 documents = []
 ignoreLetters = ['?', '!', '.', ',']
+
+for intent in intents['intents']:
+    for pattern in intent['patterns']:
+        wordList = nltk.word_tokenize(pattern)
+        words.extend(wordList)
+        documents.append((wordList, intent['tag']))
+        if intent['tag'] not in classes:
+            classes.append(intent['tag'])
+
+words = [lemmatizer.lemmatize(word) for word in words if word not in ignoreLetters]
+words = sorted(set(words))
+
+classes = sorted(set(classes))
+
+pickle.dump(words, open('words.pkl', 'wb'))
+pickle.dump(classes, open('classes.pkl', 'wb'))
+
+training = []
+outputEmpty = [0] * len(classes)
