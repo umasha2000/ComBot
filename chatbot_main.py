7abed51141a027817e202303eca7de16418bad55
Umasha Kaumadi <105776548+umasha2000@users.noreply.github.com>
2024-05-08 12:38:47 +0530

    Updated intents path

--- chatbot_main.py
+++ chatbot_main.py
@@ -8,7 +8,7 @@ from nltk.stem import WordNetLemmatizer
 from keras.models import load_model
 
 lemmatizer = WordNetLemmatizer()
-
+intents = json.loads(open('intents.json').read())
 
 words = pickle.load(open('words.pkl', 'rb'))
 classes = pickle.load(open('classes.pkl', 'rb'))
