581fcd5f44a73969e062a77f17dbf24695dfdacf
Umasha Kaumadi <105776548+umasha2000@users.noreply.github.com>
2024-05-08 12:46:13 +0530

    Response added

--- chatbot_main.py
+++ chatbot_main.py
@@ -40,3 +40,19 @@ def predict_class (sentence):
         return_list.append({'intent': classes [r[0]], 'probability': str(r[1])})
     return return_list
 
+def get_response(intents_list, intents_json):
+    tag = intents_list[0]['intent']
+    list_of_intents = intents_json['intents']
+    for i in list_of_intents:
+        if i['tag'] == tag:
+            result = random.choice (i['responses'])
+            break
+    return result
+
+print("GO! Bot is running!")
+
+while True:
+    message = input("")
+    ints = predict_class (message)
+    res = get_response (ints, intents)
+    print (res)
